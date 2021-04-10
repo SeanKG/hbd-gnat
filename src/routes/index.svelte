dda3cb4ff54f6f9b50c0de92ec8fdfbc07e52158
Sean Graves <seankgraves@gmail.com>
2021-04-10 19:38:23 -0400

    poc of intersction observer function

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,30 @@
 <script>
 	import successkid from 'images/successkid.jpg';
+
+	import IntersectionObserver from "svelte-intersection-observer";
+
+	let element;
+	let intersecting;
 </script>
+
+
+<svelte:head>
+	<title>Happy Birthday GNAT!!!?</title>
+</svelte:head>
+
+<IntersectionObserver {element} bind:intersecting threshold={1}>
+	<section>
+		<h1 bind:this={element}>Hello!!!</h1>
+		{#if intersecting}
+			<img alt="Hey" src="{successkid}">
+		{/if}
+	</section>
+</IntersectionObserver>
+
+<section>
+	<h2>This is GNAT -></h2>
+	<img alt="Gnat" src="{successkid}" class="two">
+</section>
 
 <style>
 	h1, figure, p {
@@ -12,21 +36,48 @@
 		font-size: 2.8em;
 		text-transform: uppercase;
 		font-weight: 700;
-		margin: 0 0 0.5em 0;
+		/* margin: 0 0 0.5em 0; */
 	}
 
 	figure {
 		margin: 0 0 1em 0;
 	}
 
-	img {
+	/* img {
 		width: 100%;
 		max-width: 400px;
 		margin: 0 0 1em 0;
-	}
+	} */
 
 	p {
-		margin: 1em auto;
+		/* margin: 1em auto; */
+	}
+
+	section {
+		height: 100vh;
+		width: 100vw;
+		display: flex;
+	}
+
+	h1, h2, p {
+		/* height: 100vh; */
+		width: 40vw;
+		margin-right: 60vw;
+		/* margin: auto 60vw auto 0; */
+		vertical-align: middle;
+		align-self: center;
+	}
+
+	img {
+		position: fixed;
+		top: 0;
+		right: 0;
+		height: 100vh;
+		width: 60vw;
+	}
+
+	.two {
+		opacity: 50%;;
 	}
 
 	@media (min-width: 480px) {
@@ -35,16 +86,3 @@
 		}
 	}
 </style>
-
-<svelte:head>
-	<title>Sapper project template</title>
-</svelte:head>
-
-<h1>Great success!</h1>
-
-<figure>
-	<img alt="Success Kid" src="{successkid}">
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
