5369c002956113e6d3419e438b8c24273f5442d0
Sean Graves <seankgraves@gmail.com>
2021-04-10 20:22:28 -0400

    reusable component && cleanup

---
+++ src/components/ScrollySticky.svelte
@@ -0,0 +1,46 @@
+<script>
+	import IntersectionObserver from "svelte-intersection-observer";
+
+	let element;
+	let intersecting;
+</script>
+
+<!-- <svelte:head>
+	<title>Happy Birthday GNAT!!!?</title>
+</svelte:head> -->
+
+<IntersectionObserver {element} bind:intersecting threshold={1}>
+	<section>
+        <div bind:this={element} class="scrolly">
+            <slot/>
+        </div>
+		{#if intersecting}
+            <div class="sticky">
+                <slot name="sticky" />
+            </div>
+		{/if}
+	</section>
+</IntersectionObserver>
+
+<style>
+	section {
+		height: 100vh;
+		width: 100vw;
+		display: flex;
+	}
+
+	.scrolly {
+		width: 40vw;
+		margin-right: 60vw;
+		vertical-align: middle;
+		align-self: center;
+	}
+
+	.sticky {
+		position: fixed;
+		top: 0;
+		right: 0;
+		height: 100vh;
+		width: 60vw;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,70 +1,38 @@
 <script>
 	import successkid from 'images/successkid.jpg';
-
-	import IntersectionObserver from "svelte-intersection-observer";
-
-	let element;
-	let intersecting;
+	import ScrollySticky from '../components/ScrollySticky.svelte';
+	import { fade } from 'svelte/transition';
 </script>
 
-
 <svelte:head>
-	<title>Happy Birthday GNAT!!!?</title>
+	<title>Happy Birthday GNAT!!!</title>
 </svelte:head>
 
-<IntersectionObserver {element} bind:intersecting threshold={1}>
-	<section>
-		<h1 bind:this={element}>Hello!!!</h1>
-		{#if intersecting}
-			<img alt="Hey" src="{successkid}">
-		{/if}
-	</section>
-</IntersectionObserver>
+<ScrollySticky>
+	<h1>Hello!!!</h1>
+	<div transition:fade slot="sticky">(scroll down)</div>
+</ScrollySticky>
 
-<section>
+<ScrollySticky>
 	<h2>This is GNAT -></h2>
-	<img alt="Gnat" src="{successkid}" class="two">
-</section>
+	<img transition:fade slot="sticky" alt="Gnat" src="{successkid}" class="two" >
+</ScrollySticky>
 
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
+	h1, h2, p {
 	}
 
 	h1 {
 		font-size: 2.8em;
 		text-transform: uppercase;
 		font-weight: 700;
-		/* margin: 0 0 0.5em 0; */
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	/* img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	} */
-
-	p {
-		/* margin: 1em auto; */
-	}
-
-	section {
-		height: 100vh;
-		width: 100vw;
-		display: flex;
 	}
 
 	h1, h2, p {
-		/* height: 100vh; */
+		text-align: center;
+		margin: 0 auto;
 		width: 40vw;
 		margin-right: 60vw;
-		/* margin: auto 60vw auto 0; */
-		vertical-align: middle;
 		align-self: center;
 	}
 
